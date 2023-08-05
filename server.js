872c89dab272555e8e30f537e741021f33325a20
manish511-github <singh.maneesh50@gmail.com>
2023-08-05 12:17:16 +0530

    updated

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,10 +32,10 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use("*",function(req,res)
+{
+res.sendFile(path.join(__dirname,"./client/build/index.html"));
 });
-
 //PORT
 const PORT = process.env.PORT || 8080;
 
