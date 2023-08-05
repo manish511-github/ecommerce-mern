0a5a3184fe5f11351011196e8efadfaf43ec9809
manish511-github <singh.maneesh50@gmail.com>
2023-08-05 12:05:05 +0530

    updated

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import {fileURLToPath} from "uri";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
